f7caaeaaae633fb5a9233dfbcae7d825ac1b9308
ranle <ranle336699@gmail.com>
2025-09-19 16:48:27 +0800

    配置lombok，和启动的端口号，但依然没有启动成功

--- cangqiong-server/src/main/java/com/myself/CangQiongApplication.java
+++ cangqiong-server/src/main/java/com/myself/CangQiongApplication.java
@@ -1,7 +1,14 @@
 package com.myself;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CangQiongApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CangQiongApplication.class, args);
+        log.info("CangQiongApplication Started");
+    }
 }
