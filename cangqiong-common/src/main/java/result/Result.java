4b98680655199a69a25a954cc8d8381df2ff197a
ranle <ranle336699@gmail.com>
2025-09-19 17:50:00 +0800

    写了pringweb的pom文件，对于开发模式进行dev的书写，还对返回的result类创建出来了。

---
+++ cangqiong-common/src/main/java/result/Result.java
@@ -0,0 +1,30 @@
+package result;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+    private Integer code;
+    private String msg;
+    private T data;
+
+    public static <T>Result<T> success(){
+        Result<T> result = new Result<T>();
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> success(T data){
+        Result<T> result = new Result<T>();
+        result.code = 1;
+        result.data = data;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg){
+        Result<T> result = new Result<T>();
+        result.code = 0;
+        result.msg = msg;
+        return result;
+    }
+}
