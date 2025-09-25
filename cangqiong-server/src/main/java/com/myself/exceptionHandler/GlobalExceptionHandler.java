75dcf7b265746a61c09a4c32cceb48ece8b6a1d9
ranle <ranle336699@gmail.com>
2025-09-25 18:31:08 +0800

    全局捕获用户名已存在bug

--- cangqiong-server/src/main/java/com/myself/exceptionHandler/GlobalExceptionHandler.java
+++ cangqiong-server/src/main/java/com/myself/exceptionHandler/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.myself.exceptionHandler;
 
+import constant.MessageConstant;
 import result.Result;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 
 @RestControllerAdvice
@@ -12,4 +15,19 @@ public class GlobalExceptionHandler {
     public Result<String> handleException(Exception e) {
         return Result.error(e.getMessage());
     }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public Result<String> handleException(SQLIntegrityConstraintViolationException e) {
+
+        String message = e.getMessage();
+
+        if (message.contains("Duplicate entry")) {
+            String[] split = message.split("");
+            return Result.error(split[2]+ MessageConstant.USERNAME_IS_EXIST);
+        }else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+
+    }
+
 }
